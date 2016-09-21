7c71d32af9f417bbd31d98acdc2ff6e2d2fb4f68
Safwat Shenouda <bingoapps@hotmail.com>
2016-09-22 00:37:01 +0200

    initial

---
+++ AmuesmentParkPassGenerator/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  AmuesmentParkPassGenerator
+//
+//  Created by Safwat Shenouda on 22/09/16.
+//  Copyright © 2016 Safwat Shenouda. All rights reserved.
+//
+
+import Foundation
